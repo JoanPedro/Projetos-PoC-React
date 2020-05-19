11bc49a4d8007c782b1f2cc42b4c019e69b28990
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-19 12:05:39 -0300

    Basic

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,24 @@
+import './index.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+// npx create-react-app <nome da paste que será criada>
+/* Método Render - 
+2 Parâmetros: 
+1. Elemento que quer renderizar em tela, 2. Elemento onde será inserido.
+Inserido no Index.html -> <div id="root"> </div>
+*/
+
+/* Método 1:
+const el = document.getElementById('root')
+ReactDOM.render('Olá React', el) */
+
+// Método 2: -> String dentro do DOM.
+const tag = <strong>Olá React!!!</strong>;
+
+ReactDOM.render(
+  <div>
+    { tag }
+  </div>,
+  document.getElementById('root'),
+);
