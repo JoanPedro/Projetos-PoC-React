0ce15dfa226f3d487aa448d06046094301a2789c
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-19 14:17:30 -0300

    Componente Número Aleatório

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Primeiro from './components/basics/Primeiro';
 import Fragmento from './components/basics/Fragments';
 import SegundoComParametro from './components/basics/SegundoComParametro';
+import Aleatorio from './components/basics/Aleatorio';
 
 export default _ => {
   const tag = <strong>Olá React!!!</strong>;
   return (
     <div>
       <h1>Fundamentos React</h1>
+      < Aleatorio 
+      min = { 1000 }
+      max = { 2000 }
+      />
       { tag }
       <p />
       <Fragmento />
