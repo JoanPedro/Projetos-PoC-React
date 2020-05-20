e8636153c8075ceae689f09a35e935795caa344a
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-20 14:54:47 -0300

    Componente pai com Estado! (useState)

--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IndiretaFilho from './IndiretaFilho';
 
 export default (props) => {
 
+  let [nome, setNome] = useState('?'); 
+  let [idade, setIdade] = useState(0); 
+  let [nerd, setNerd] = useState(false);
+
   const fornecerInformacoes = (nome, idade, nerd) => {
-    console.log(nome, idade, nerd)
+    setNome(nome);
+    setIdade(idade);
+    setNerd(nerd);
+
+    console.log(nerd, idade, nome)
   }
 
   return(
     <div>
       <div>Pai</div>
+      <span>{nome} <strong>{idade}</strong> {`Nerd: ${nerd}`}</span>
       <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
     </div>
   )
